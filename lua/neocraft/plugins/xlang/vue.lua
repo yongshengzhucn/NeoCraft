bcec8269d783cc834dfb064b85b010b31ef05b9b
Neo <zhuyongshengcn@gmail.com>
2025-06-06 14:21:14 +0800

    Updates

--- lua/neocraft/plugins/xlang/vue.lua
+++ lua/neocraft/plugins/xlang/vue.lua
@@ -1,42 +1,42 @@
 return {
-	-- depends on the typescript extra
+  -- depends on the typescript extra
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = { ensure_installed = { "vue", "css" } },
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "vue", "css" } },
+  },
 
-	-- Add LSP servers
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				volar = {
-					init_options = {
-						vue = {
-							hybridMode = true,
-						},
-					},
-				},
-				vtsls = {},
-			},
-		},
-	},
+  -- Add LSP servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        volar = {
+          init_options = {
+            vue = {
+              hybridMode = true,
+            },
+          },
+        },
+        vtsls = {},
+      },
+    },
+  },
 
-	-- Configure tsserver plugin
-	{
-		"neovim/nvim-lspconfig",
-		opts = function(_, opts)
-			table.insert(opts.servers.vtsls.filetypes, "vue")
-			NeoCraft.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
-				{
-					name = "@vue/typescript-plugin",
-					location = NeoCraft.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-					languages = { "vue" },
-					configNamespace = "typescript",
-					enableForWorkspaceTypeScriptVersions = true,
-				},
-			})
-		end,
-	},
+  -- Configure tsserver plugin
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+      NeoCraft.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = NeoCraft.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
+  },
 }
